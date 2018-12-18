8119098d8622bdaf591f4650a2390e7c9620b12d
fanxl12 <744822514@qq.com>
2018-12-18 22:05:46 +0800

    Feign配置Hystrix;

--- client/src/main/java/com/fanxl/product/client/ProductClient.java
+++ client/src/main/java/com/fanxl/product/client/ProductClient.java
@@ -3,6 +3,7 @@ package com.fanxl.product.client;
 import com.fanxl.product.common.DecreaseStockInput;
 import com.fanxl.product.common.ProductInfoOutput;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ import java.util.List;
  * @author: fanxl
  * @date: 2018/10/26 0026 11:39
  */
-@FeignClient(name = "product")
+@FeignClient(name = "product", fallback = ProductClient.ProductFallBack.class)
 public interface ProductClient {
 
     @GetMapping("product/listForOrder")
@@ -27,5 +28,23 @@ public interface ProductClient {
     @GetMapping("test/{name}")
     String getTest(@PathVariable("name") String name);
 
+    @Component
+    class ProductFallBack implements ProductClient {
+
+        @Override
+        public List<ProductInfoOutput> listForOrder(List<String> productIdList) {
+            return null;
+        }
+
+        @Override
+        public void decreaseStock(List<DecreaseStockInput> decreaseStockInputList) {
+
+        }
+
+        @Override
+        public String getTest(String name) {
+            return null;
+        }
+    }
 
 }
